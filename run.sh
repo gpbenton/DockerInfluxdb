cd5890ecf685792db832c25c36a0f620b9f10775
Graham <github.gpbenton@xoxy.net>
2017-11-09 09:02:08 +0000

    Use renamed hub item

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d --name=influxdb -p 8086:8086 -p 8088:8088 -v /home/graham/influxdb:/etc/influxdb:ro -v influxdbdata:/var/lib/influxdb gpbenton/dockerinfluxdb
+docker run -d --name=influxdb -p 8086:8086 -p 8088:8088 -v /home/graham/influxdb:/etc/influxdb:ro -v influxdbdata:/var/lib/influxdb gpbenton/i386-influxdb
